fc8b0098a1535d0573e620fe98fe94a54a118843
Ful Belin Korukoglu <70371170+fulbelin@users.noreply.github.com>
2023-05-16 17:30:51 +0300

    412_FizzBuzz.py

---
+++ 412_FizzBuzz.py
@@ -0,0 +1,21 @@
+#Given an integer n, return a string array answer (1-indexed) where:
+#answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+#answer[i] == "Fizz" if i is divisible by 3.
+#answer[i] == "Buzz" if i is divisible by 5.
+#answer[i] == i (as a string) if none of the above conditions are true.
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        list = []
+        for i in range(1, n+1):
+            if i % 15 == 0 :
+                list.append('FizzBuzz') 
+            elif i % 3 == 0:
+                list.append('Fizz')
+            elif i % 5 == 0:
+                list.append('Buzz')
+         
+            else:
+                list.append(str(i))          
+        return list
+ 
